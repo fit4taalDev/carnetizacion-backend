74d115d83e8b2a60dc352a50916dc8204f08113c
fit4taalDev <pablo@fit4taal.com>
2025-04-23 05:50:12 -0500

    Initial project configuration

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express()
+
+const PORT  = process.env.PORT || 3000;
+app.use(express.json());
+
+const main = async () => {
+    try{
+        app.listen(PORT, async() => {
+            console.log('Database connected');
+            console.log(`Server is running on http://localhost:${PORT}`)
+        })
+    }catch(error){
+        console.log(error)
+    }
+}
+
+main()
