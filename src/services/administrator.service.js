46758b9c12b7b6738e5c64dd119a9ce094c3ca5e
fit4taalDev <pablo@fit4taal.com>
2025-04-27 10:39:29 -0500

    fix administrator service

--- src/services/administrator.service.js
+++ src/services/administrator.service.js
@@ -2,6 +2,7 @@ import { Administrators } from "../database/models/administrators.model.js";
 import { Users } from "../database/models/users.model.js";
 import bcrypt from 'bcrypt'
 import BaseService from "./base.service.js";
+import { sequelize } from "../database/sequelize.js";
 
 class AdministratoService extends BaseService{
     constructor(){
@@ -15,19 +16,29 @@ class AdministratoService extends BaseService{
         }
     }
 
-    async createAdministrator(data){
-        const userData = {
-            email: data.email,
-            password: await bcrypt.hash(data.password, 10),
-            role_id: 0
-        }
-        await this.checkIfExistIn(Users, 'email', data.email, 'The email address is already registered');
+    async createAdministrator(data) {
+        return await sequelize.transaction(async (t) => {
+            const userData = {
+                email: data.email,
+                password: await bcrypt.hash(data.password, 10),
+                role_id: 0
+            };
 
-        const newUser = await Users.create(userData)
+            await this.checkIfExistIn(Users, 'email', data.email, 'The email address is already registered', { transaction: t });
+            await this.checkIfExistIn(Administrators, 'phone_number', data.phone_number, 'The phone number is already registered', { transaction: t });
 
-        const newAdminData = {...data,id:newUser.id ,user_id: newUser.id}
+            const newUser = await Users.create(userData, { transaction: t });
 
-        return super.create(newAdminData)
+            const newAdminData = {
+                ...data,
+                id: newUser.id,
+                user_id: newUser.id
+            };
+
+            const newAdmin = await Administrators.create(newAdminData, { transaction: t });
+
+            return newAdmin;
+        });
     }
     
 }
