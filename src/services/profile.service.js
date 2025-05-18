7d0e43d12031e1fe874d985ff26da26675875f71
fit4taalDev <pablo@fit4taal.com>
2025-05-18 10:36:51 -0500

    get profile information

--- src/services/profile.service.js
+++ src/services/profile.service.js
@@ -15,24 +15,36 @@ export default class ProfileService extends BaseService {
     super(Users)
   }
 
-  async findProfile(user) {
-    const include = []
-    switch (user.role_id) {
-      case ROLE.ADMIN:
-        include.push({ model: Administrators,  as: 'administrator' })
-        break
-      case ROLE.ESTABLISHMENT:
-        include.push({ model: Establishments,  as: 'establishment' })
-        break
-      case ROLE.STUDENT:
-        include.push({ model: Students,        as: 'student' })
-        break
+   async findProfile(id) {
+    // 1) Traer al usuario sin password para conocer su role_id
+    const user = await this.model.findByPk(id, {
+      attributes: { exclude: ["password"] }
+    });
+    if (!user) {
+      const err = new Error("User not found");
+      err.status = 404;
+      throw err;
     }
 
+    // 2) Montar el include según su rol
+    const include = [];
+    switch (user.role_id) {
+      case ROLE.ADMIN:
+        include.push({ model: Administrators,  as: "administrator" });
+        break;
+      case ROLE.ESTABLISHMENT:
+        include.push({ model: Establishments,  as: "establishment" });
+        break;
+      case ROLE.STUDENT:
+        include.push({ model: Students,        as: "student" });
+        break;
+    }
+
+    // 3) Devolver el perfil completo (sin password) con su tabla asociada
     return this.model.findOne({
-      where:      { id: user.id },
-      attributes: { exclude: ['password'] }, 
+      where:      { id },
+      attributes: { exclude: ["password"] },
       include
-    })
+    });
   }
 }
